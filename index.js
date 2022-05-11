22edf86076d6e8872a21bf8b7741c6ffbfa2aed1
monichamcham <monica.leyton1601@alumnos.ubiobio.cl>
2022-05-11 01:14:00 -0400

    Página principal, aún falta contenido en ésta y funciones de js

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ window.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     header.classList.toggle("abajo", window.scrollY > 0);
 })
+
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+searchButton.addEventListener('click', () => {
+  const inputValue = searchInput.value;
+  alert(inputValue);
+});
