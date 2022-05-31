ff8537f5b4471e64d7efc7c9ae79ef169dca1c92
monichamcham <monica.leyton1601@alumnos.ubiobio.cl>
2022-05-31 11:43:01 -0400

    Se implementa caledario para la vista del administrador (falta crud para eventos por día)

---
+++ Calendario_admin/Controllers/Home.php
@@ -0,0 +1,10 @@
+<?php
+class Home extends Controller
+{
+    public function __construct() {
+        parent::__construct();
+    }
+    public function index(){
+        $this->views->getView($this, 'index');
+    }
+}
