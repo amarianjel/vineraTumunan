de490cec83d3e5e9a9a9a8f806833646f20f69e9
monichamcham <monica.leyton1601@alumnos.ubiobio.cl>
2022-06-01 20:55:58 -0400

    Se agrega formulario de reservas y por acuerdo se dehacen avances creados anteriormente

--- Calendario_admin/Config/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-const base_url = "http://localhost/Calendario_admin/";
-const host = "127.0.0.1: 3308";
-const user = "root";
-const pass = "";
-const db = "agendas";
-const charset = "charset=utf8";
-?>
