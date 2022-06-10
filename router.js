ad523f59151968795548cbda2106855c2850d46b
amarianjel <abraham.marianjel@gmail.com>
2022-06-10 16:13:11 -0400

    Ajustando los colores del carrusel

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const dotenv = require('dotenv');
-dotenv.config({ path: './env/.env'});
+dotenv.config({ path: './env/.env' });
 
 //Invocamos a bcrypt
 const bcrypt = require('bcryptjs');
@@ -10,41 +10,41 @@ const bcrypt = require('bcryptjs');
 //Variables de session
 const session = require('express-session');
 router.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
 }));
 
 const conexion = require('./database/db');
 
 
 //RUTA PARA LANZAR EL HOME
-router.get('/', (req,res)=>{
+router.get('/', (req, res) => {
     res.render('index');
 })
 
-router.get('/catalogo', (req,res)=>{
-	conexion.query('SELECT * FROM productos WHERE estado="activo"', (error,results)=>{
-		if(error){
-			throw error;
-		}else{
-			res.render('catalogoProductos',{results:results});
-		}
-	   })
-    
+router.get('/catalogo', (req, res) => {
+    conexion.query('SELECT * FROM productos WHERE estado="activo"', (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            res.render('catalogoProductos', { results: results });
+        }
+    })
+
 })
 
 
 //
-router.get('/data', (req,res)=>{
-    conexion.query('SELECT * FROM productos WHERE estado="activo"', (error,results)=>{
-       if(error){
-           throw error;
-       }else{
-           data = JSON.stringify(results)
+router.get('/data', (req, res) => {
+    conexion.query('SELECT * FROM productos WHERE estado="activo"', (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            data = JSON.stringify(results)
             res.send(data);
-       }
-  	})
+        }
+    })
 })
 
 
