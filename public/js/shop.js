84bcb783b872b1d238362e404e56175cb87f6a4f
roloGomez <rodrigo.gomez1701@alumnos.ubiobio.cl>
2022-06-18 18:10:30 -0400

    Vista de carrito y algunas funciones

---
+++ public/js/shop.js
@@ -0,0 +1,53 @@
+var cont = 0;
+var total = 0;
+const table = document.getElementById("testBody");
+let pagar = document.getElementById("total_buy");
+
+function loadTableData(items) {
+    items.forEach( item => {
+    let row = table.insertRow();
+    row.id = "row "+cont;
+    let name = row.insertCell(0);
+    name.innerHTML = item.name;
+    let date = row.insertCell(1);
+    date.innerHTML = item.date;
+    let price = row.insertCell(2);
+    price.innerHTML = item.price;
+    price.id = "precio";
+    total = total+parseInt(item.price);
+    let but = row.insertCell(3);
+    but.innerHTML = "<button class='btn-dark'>X</button>";
+    cont++;
+    })
+    pagar.innerHTML = total;
+}
+//Eliminar Fila de la Tabla
+table.addEventListener('click', e => {
+    if(e.target.classList.contains('btn-dark')){
+        delet(e.target.parentElement.parentElement.id); //se pasa el id de la fila contenedora del Boton
+    }
+    e.stopPropagation();
+});
+function delet(id){
+    //Se almacena el precio para luego descontarlo del valor total a pagar y actualizarlo
+    let price = document.getElementById(id).querySelector('#precio').textContent;
+    total=total-parseInt(price);
+    pagar.innerHTML = total;
+    //Eliminando la Fila de la Tabla
+    document.getElementById(id).remove();
+}
+//Datos de Prueba
+const items1 = [
+    { name: "vino 1", date: "10", price: "50000" },
+    {  name: "vino 2", date: "18", price: "34000"  },
+];
+const items2 = [
+    {  name: "vino 4", date: "8", price: "150000" },
+    {  name: "vino 3", date: "1", price: "20000"  },
+];
+loadTableData(items1);
+loadTableData(items2);
+loadTableData(items2);
+loadTableData(items1);
+loadTableData(items2);
+loadTableData([]);
---
+++ router.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+
+const dotenv = require('dotenv');
+dotenv.config({ path: './env/.env' });
+
+//Invocamos a bcrypt
+const bcrypt = require('bcryptjs');
+
+//Variables de session
+const session = require('express-session');
+router.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+const conexion = require('./database/db');
+
+
+//RUTA PARA LANZAR EL HOME
+router.get('/', (req, res) => {
+    res.render('index');
+})
+
+
+router.get('/catalogo', (req, res) => {
+    conexion.query('SELECT * FROM productos WHERE estado="activo"', (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            res.render('catalogoProductos', { results: results });
+        }
+    })
+
+})
+router.get('/galeria', (req, res) => {
+    res.render('galeria');
+})
+
+//
+router.get('/data', (req, res) => {
+    conexion.query('SELECT * FROM productos WHERE estado="activo"', (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            data = JSON.stringify(results)
+            res.send(data);
+        }
+    })
+})
+
+router.get('/shop', (req, res) => {
+    res.render('shop');
+})
+router.get('/test', (req, res) =>{
+    res.render('shop.js');
+})
+
+module.exports = router;
