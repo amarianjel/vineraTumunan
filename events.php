696ff7121ce22c851a11d5722d153423f5326ee0
roloGomez <rodrigo.gomez1701@alumnos.ubiobio.cl>
2022-09-05 12:01:23 -0300

    correccion titulos

--- events.php
+++ events.php
@@ -10,7 +10,7 @@ require 'config/config.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tumuñan Eventos</title>
+    <title>Viña Tumuñan Lodge | Eventos</title>
 
     <!--Style-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
