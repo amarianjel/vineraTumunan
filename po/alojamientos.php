a6e72fd3acab077675e3ab3133ff7ea31d0931a0
amarianjel <abraham.marianjel@gmail.com>
2022-10-17 10:19:35 -0300

    Cambios de orden de las imagenes y tipo de alojamiento, cambio de numero de Whatsapp y redireecion a otra pagina

--- po/alojamientos.php
+++ po/alojamientos.php
@@ -55,7 +55,7 @@ require '../config/config.php';
 
 </head>
 <body>
-    <a href="https://wa.me/56996301152" class="btn-wsp" target="_blank">
+    <a href="https://wa.me/56987875048" class="btn-wsp" target="_blank">
         <i class="fa fa-whatsapp icono"></i>
     </a>
     <!-- header -->
@@ -141,79 +141,46 @@ require '../config/config.php';
 	</div>
 	<!-- end hero area -->
 
+
+    <style>
+        /*********Alojamientos***********/
+        .titulo {
+            color: #051922;
+        }
+
+        @media only screen and (max-width: 959px) {
+            body:before {
+                content: "";
+                background-image: url('your background url goes here');
+                background-size: cover;
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+            }
+        }
+    </style>
+    <!-- main section -->
     <main>
-        <section>
+        <section class="center espacio mx-0 my-5">
+            <hr>
             <div class="row d-flex justify-content-center">
-                <div class="col-8">
+                <div class="col-10">
                     <h2>Desconecte em Alto Colchagua</h2>
+                    <br><br>
                     <div>
                         <p class="justificar-inicio">Temos trabalhado arduamente para reconfigurar o Tumuñan Lodge para poder receber os hóspedes de forma higiênica e segura. Aluguéis mensais são apenas uma maneira de alugar uma de nossas 3 cabines totalmente equipadas.
                         </p>
                     </div>
+                    <br><br>
                 </div>
             </div>
-        </section>
-        <section>
-            <div class="container container2">
-                <div class="row">
-                    <div class="col-md-7">
-                        <div id="slider">
-                            <input type="radio" name="slider" id="s1" checked>
-                            <input type="radio" name="slider" id="s2">
-                            <input type="radio" name="slider" id="s3">
-                            <input type="radio" name="slider" id="s4">
-                            <input type="radio" name="slider" id="s5">
-            
-                            <label for="s1" id="slide1">
-                            <img class="carrusel" src="../images/bed/Rooms Website/casa/IMG-20200519-WA0000.jpg">
-                        </label>
-                            <label for="s2" id="slide2">
-                            <img class="carrusel" src="../images/bed/Rooms Website/casa/IMG-20200519-WA0001.jpg">
-                        </label>
-                            <label for="s3" id="slide3">
-                            <img class="carrusel" src="../images/bed/Rooms Website/casa/IMG-20200519-WA0003.jpg">
-                        </label>
-                            <label for="s4" id="slide4">
-                            <img class="carrusel" src="../images/bed/Rooms Website/casa/IMG-20200519-WA0005.jpg">
-                        </label>
-                            <label for="s5" id="slide5">
-                            <img class="carrusel" src="../images/bed/IMG_4327.jpg">
-                        </label>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-md-4 offset-md-1">
-                        <h1 class="titulo">Chalet</h1>
-                        <p class="justificar-parrafo">
-                        Espaçosa casa de 4 quartos com cozinha totalmente equipada, pátio privativo, jardim, fogueira, churrasqueira e vistas espetaculares da montanha.
-                        </p>
-                        <h4>Preço</h4>
-                        <p class="costos">
-                            CLP$410,000 por fim-de-semana (2 noites/ 3 dias, prova de vinhos e 1 dia de uso do jacuzzi incluído).
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <hr>
-        <section>
-            <div class="container .container2">
-                <div class="row">
-                    <div class="col-md-4">
-                        <h1 class="titulo">Chalet para Casais</h1>
-                        <p class="justificar-parrafo">
-                        Cabana moderna que tem 1 quarto com jardins privativos, fogueira, churrasqueira e uma pequena cozinha. Perto do rio e com vista serra.
-                        </p>
-                        <h4>Preços</h4>
-                        <p class="costos">
-                            CLP$240,000 por fim-de-semana (2 noites/ 3 dias, prova de vinhos e 1 dia de uso do jacuzzi incluído).
-                        </p>
-                        <hr>
-                        <p class="costos">
-                            CLP$510,000 por semana (5 noites / 6 dias, passeio e prova de vinhos e 2 dias de uso do jacuzzi incluídos).
-                        </p>
-                    </div>
-                    <div class="col-md-7 offset-md-1">
+            <br>
+            <hr><br>
+            <div class="row align-items-center">
+                <div class="fotografia-info col-12 col-lg-6 my-5">
+                    <div class="row">
                         <div id="slider2">
                             <input type="radio" name="slider" id="s6">
                             <input type="radio" name="slider" id="s7">
@@ -222,82 +189,148 @@ require '../config/config.php';
                             <input type="radio" name="slider" id="s10">
 
                             <label for="s6" id="slide6">
-                        <img class="carrusel" src="../images/bed/chalet_parejas/1.jpeg">
-                    </label>
+                                <img class="carrusel" src="../images/bed/chalet_parejas/1.jpeg">
+                            </label>
                             <label for="s7" id="slide7">
                                 <img class="carrusel" src="../images/bed/chalet_parejas/2.jpeg">
-                    </label>
+                            </label>
                             <label for="s8" id="slide8">
-                        <img src="../images/bed/chalet_parejas/3.jpeg">
-                    </label>
+                                <img class="carrusel" src="../images/bed/chalet_parejas/3.jpeg">
+                            </label>
                             <label for="s9" id="slide9">
-                        <img class="carrusel" src="../images/bed/chalet_parejas/4.jpeg">
-                    </label>
+                                <img class="carrusel" src="../images/bed/chalet_parejas/4.jpeg">
+                            </label>
                             <label for="s10" id="slide10">
                                 <img class="carrusel" src="../images/bed/chalet_parejas/6.jpeg">
-                    </label>
+                            </label>
                             </p>
                         </div>
                     </div>
                 </div>
+                <div class="info col-12 col-lg-6">
+                    <h1 class="titulo">Chalet para Casais</h1>
+                    <p style="font-size: 19px">
+                        Cabana moderna que tem 1 quarto com jardins privativos, fogueira, churrasqueira e uma pequena cozinha. Perto do rio e com vista serra.
+                    </p>
+                    <h4>Preços</h4>
+                    <p style="font-size: 19px">
+                            CLP$240,000 por fim-de-semana (2 noites/ 3 dias, prova de vinhos e 1 dia de uso do jacuzzi incluído).
+                    </p>
+                    <hr>
+                    <p style="font-size: 19px">
+                            CLP$510,000 por semana (5 noites / 6 dias, passeio e prova de vinhos e 2 dias de uso do jacuzzi incluídos).
+                    </p>
+                </div>
             </div>
-        </section>
-        <hr>
-        <section>
-            <div class="container container2">
-                <div class="row">
-                    <div class="col-md-7">
+            <br>
+
+            <hr>
+            <br>
+
+            <div class="row align-items-center">
+                <div class="info col-12 col-lg-6">
+                    <h2 class="titulo">Chalet para familia</h2>
+                    <p style="font-size: 19px">
+                        Espaçosa casa de 4 quartos com cozinha totalmente equipada, pátio privativo, jardim, fogueira, churrasqueira e vistas espetaculares da montanha.
+                    </p>
+                    <h4>Preço</h4>
+                    <p style="font-size: 19px">
+                            CLP$410,000 por fim-de-semana (2 noites/ 3 dias, prova de vinhos e 1 dia de uso do jacuzzi incluído).
+                    </p>
+                </div>
+                <div class="fotografia-info col-12 col-lg-6 my-5">
+                    <div class="row">
+                        <div id="slider">
+                            <input type="radio" name="slider" id="s1" checked>
+                            <input type="radio" name="slider" id="s2">
+                            <input type="radio" name="slider" id="s3">
+                            <input type="radio" name="slider" id="s4">
+                            <input type="radio" name="slider" id="s5">
+
+                            <label for="s1" id="slide1">
+                                <img class="carrusel" src="../images/bed/chalet2020/DSC_0666.jpg">
+                            </label>
+                            <label for="s2" id="slide2">
+                                <img class="carrusel" src="../images/bed/chalet2020/DSC_0598.jpg">
+                            </label>
+                            <label for="s3" id="slide3">
+                                <img class="carrusel" src="../images/bed/chalet/King Suite 4.jpg">
+                            </label>
+                            <label for="s4" id="slide4">
+                                <img class="carrusel" src="../images/bed/chalet/King Suite 6.jpg">
+                            </label>
+                            <label for="s5" id="slide5">
+                                <img class="carrusel" src="../images/bed/chalet/King Suite 7.jpg">
+                            </label>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <br>
+            <hr>
+            <br>
+
+            <div class="row align-items-center">
+                <div class="fotografia-info col-12 col-lg-6 my-5">
+                    <div class="row">
                         <div id="slider3">
                             <input type="radio" name="slider" id="s11">
                             <input type="radio" name="slider" id="s12">
                             <input type="radio" name="slider" id="s13">
                             <input type="radio" name="slider" id="s14">
                             <input type="radio" name="slider" id="s15">
-            
+
                             <label for="s11" id="slide11">
-                            <img class="carrusel" src="../images/bed/casa_montana/1.jpg">
-                        </label>
+                                <img class="carrusel" src="../images/bed/casa_montana/1.jpg">
+                            </label>
                             <label for="s12" id="slide12">
-                            <img class="carrusel" src="../images/bed/casa_montana/2.jpg">
-                        </label>
+                                <img class="carrusel" src="../images/bed/casa_montana/2.jpg">
+                            </label>
                             <label for="s13" id="slide13">
-                            <img class="carrusel" src="../images/bed/casa_montana/3.jpg">
-                        </label>
+                                <img class="carrusel" src="../images/bed/casa_montana/3.jpg">
+                            </label>
                             <label for="s14" id="slide14">
-                            <img class="carrusel" src="../images/bed/casa_montana/4.jpg">
-                        </label>
+                                <img class="carrusel" src="../images/bed/casa_montana/4.jpg">
+                            </label>
                             <label for="s15" id="slide15">
-                            <img class="carrusel" src="../images/bed/casa_montana/5.jpg">
-                        </label>
+                                <img class="carrusel" src="../images/bed/casa_montana/5.jpg">
+                            </label>
                             </p>
                         </div>
                     </div>
-                    <div class="col-md-4 offset-md-1">
-                        <h1 class="titulo">Casa da Montanha</h1>
-                        <p class="justificar-parrafo">
-                        Cabine moderna e aconchegante com 2 quartos, 2 banheiros, fogueira, churrasqueira e cozinha compacta. Perto do rio com vista para a montanha.
-                        </p>
-                        <h4>Preços</h4>
-                        <p class="costos">
-                            CLP$360,000 por fim-de-semana (2 noites/ 3 dias, prova de vinhos e 1 dia de utilização do jacuzzi incluídos).
-                        </p>
-                        <hr>
-                        <p class="costos">
-                            CLP$720,000 por semana (5 noites / 6 dias, tour e prova de vinhos e 2 dias de uso do jacuzzi incluídos).
-                        </p>
-                    </div>
                 </div>
-            </div>
+                <div class="info col-12 col-lg-6">
+                    <h1 class="titulo">Casa da Montanha</h1>
+                    <p style="font-size: 19px">
+                    Cabine moderna e aconchegante com 2 quartos, 2 banheiros, fogueira, churrasqueira e cozinha compacta. Perto do rio com vista para a montanha.
+                    </p>
+                    <h4>Preços</h4>
+                    <p style="font-size: 19px">
+                        CLP$360,000 por fim-de-semana (2 noites/ 3 dias, prova de vinhos e 1 dia de utilização do jacuzzi incluídos).
+                    </p>
+                    <hr>
+                    <p style="font-size: 19px">
+                        CLP$720,000 por semana (5 noites / 6 dias, tour e prova de vinhos e 2 dias de uso do jacuzzi incluídos).
+                    </p>
+                </div>
         </section>
-        <section class="center espacio">
-            <h3>Reserve agora</h3>
-            <div>
-                <p>Para reservar um quarto, envie um email para  <a href="./formReservas.php">Clique aqui</a></p>
-                <p>O que está incluído: Acesso à piscina e jacuzzi, trilhas, Wi-Fi, lençóis e travesseiros (TV direta na casa da montanha).</p>
-                <p> O que não está incluso: serviço de limpeza diário, limpeza, café da manhã, restaurante A tarifa também inclui: Toalhas de banho e de mão (1 por pessoa) Produtos básicos de cozinha (Lavalozas, sacos de lixo, Sal).</p>
+        <hr>
+        <br>
+        <h2 class="titulo" style="display: flex; justify-content: center;  background-color: #000000e7; color: #fff;">Ubicación</h2>
+        <div class="mapa" style="display: flex; justify-content: center; margin:1em;">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21963.498815824205!2d-70.79943981194643!3d-34.7499001301102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9664c1b33eefd169%3A0x7c7f01fc0de1ef70!2sTumu%C3%B1an%20Lodge!5e0!3m2!1ses-419!2scl!4v1659462547925!5m2!1ses-419!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <!--Video-->
+        <section>
+            <div class="ratio ratio-21x9 alto-completo">
+                <iframe width="100%" height="100%" src="../images/video_vina.mp4" title="Viña Tumuñan" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <!--<iframe src="https://www.youtube.com/embed/_Y9afSciAAM?autoplay=1&mute=1 " title="YouTube video player " frameborder="0 " allowfullscreen></iframe>-->
             </div>
         </section>
     </main>
+    <!-- end main section -->
 
     <?php  include("../includes/po/footer.php");?>
 </body>
